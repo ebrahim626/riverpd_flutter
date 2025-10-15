1a71b853dcec4c66439e4ee047ad0da90c869eb3
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-15 19:55:56 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+final hello = Provider<String>((ref) {
+  return "Subscribers";
+});
+
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final text = ref.watch(hello);
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text('data')),
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Riverpod Test",
+            style: TextStyle(
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+              fontSize: 25,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.lightBlue,
+      ),
+      body: Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 40, color: Colors.black54),
+        ),
+      ),
     );
   }
 }
