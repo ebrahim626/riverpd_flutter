dd02bf392ad6ce1348be112cba839cab2a62ea07
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-15 20:14:35 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 final hello = Provider<String>((ref) {
   return "Subscribers";
+});
+
+final counter = StateProvider<int>((ref) {
+  return 0;
 });
 
 class HomeScreen extends ConsumerWidget {
@@ -11,6 +16,7 @@ class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final text = ref.watch(hello);
+    final count = ref.watch(counter);
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -26,9 +32,32 @@ class HomeScreen extends ConsumerWidget {
         backgroundColor: Colors.lightBlue,
       ),
       body: Center(
-        child: Text(
-          text,
-          style: TextStyle(fontSize: 40, color: Colors.black54),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$text $count",
+              style: TextStyle(fontSize: 40, color: Colors.black54),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counter.notifier).state--;
+                  },
+                  child: Icon(Icons.remove, size: 25),
+                ),
+                SizedBox(width: 15),
+                ElevatedButton(
+                  onPressed: () {
+                    ref.read(counter.notifier).state++;
+                  },
+                  child: Icon(Icons.add, size: 25),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
