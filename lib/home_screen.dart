7f3c77d15097f6cec346d983fdf3f35051119dfe
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-10-15 20:52:45 +0600

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
@@ -12,11 +11,22 @@ final counter = StateProvider<int>((ref) {
   return 0;
 });
 
-class HomeScreen extends ConsumerWidget {
+final switchProvider = StateProvider<bool>((ref){
+  return false;
+});
+
+
+
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
     final text = ref.watch(hello);
     log('message1');
     return Scaffold(
@@ -40,18 +50,18 @@ class HomeScreen extends ConsumerWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-              Text(text,style: TextStyle(fontSize: 40, color: Colors.black54)),
-              Consumer(
-                builder: (BuildContext context, WidgetRef ref, Widget? child) {
-                  log('message2');
-                  final count = ref.watch(counter);
-                  return Text(
-                    " $count",
-                    style: TextStyle(fontSize: 40, color: Colors.black54),
-                  );
-                },
-              ),
-            ],),
+                Text(text,style: TextStyle(fontSize: 40, color: Colors.black54)),
+                Consumer(
+                  builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                    log('message2');
+                    final count = ref.watch(counter);
+                    return Text(
+                      " $count",
+                      style: TextStyle(fontSize: 40, color: Colors.black54),
+                    );
+                  },
+                ),
+              ],),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -71,9 +81,19 @@ class HomeScreen extends ConsumerWidget {
                 ),
               ],
             ),
+            Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                log('message3');
+                final toggleSwitch = ref.watch(switchProvider);
+                return Switch(value: toggleSwitch, onChanged: (value){
+                  ref.read(switchProvider.notifier).state = value;
+                });
+              },
+            ),
           ],
         ),
       ),
     );
   }
 }
+
